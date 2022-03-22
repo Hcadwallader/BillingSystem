9b4f1c310edc6e1c2476a4c6c3b9f488d0bc064c
Hcadwallader <helenschwan3@gmail.com>
2022-03-22 19:23:37 +0000

    add charge tests

---
+++ models/__tests__/charge.test.js
@@ -0,0 +1,36 @@
+import { beforeEach } from '@jest/globals';
+import Charge from '../charge.js';
+
+describe('Constructor', () => {
+	test('Minimum number of params provided', () => {
+		let charge = new Charge('2022-02-01', 1000, 2, 1);
+
+		expect(charge.date).toBe('2022-02-01');
+		expect(charge.amount).toBe(1000);
+		expect(charge.mandateId).toBe(2);
+		expect(charge.advanceId).toBe(1);
+		expect(charge.finalPayment).toBe(false);
+		expect(charge.succeeded).toBe(false);
+	});
+	test('Override defaults if provide optional params', () => {
+		let charge = new Charge('2022-02-01', 1000, 2, 1, true, true);
+
+		expect(charge.date).toBe('2022-02-01');
+		expect(charge.amount).toBe(1000);
+		expect(charge.mandateId).toBe(2);
+		expect(charge.advanceId).toBe(1);
+		expect(charge.finalPayment).toBe(true);
+		expect(charge.succeeded).toBe(true);
+	});
+});
+
+describe('Mark as successful works correctly', () => {
+	test('Update charge to successful when paid', () => {
+		let charge = new Charge('2022-02-01', 1000, 2, 1);
+		expect(charge.succeeded).toBe(false);
+
+		charge.markAsSuccessful();
+
+		expect(charge.succeeded).toBe(true);
+	});
+});
